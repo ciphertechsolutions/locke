4999870c7312fa5d9149df4aab0105eaad8891b9
William Woodruff <wwoodruff@ciphertechsolutions.com>
2017-07-20 14:52:06 -0400

    pattern: Stub out pattern subclasses

--- locke/pattern.py
+++ locke/pattern.py
@@ -1,6 +1,23 @@
+"""
+This module contains definitions for the Pattern class and its immediate
+descendants.
+"""
+
+
 from abc import ABC, abstractmethod
 
+
 class Pattern(ABC):
+    """
+    This is the abstract class for all Locke patterns.
+    """
+    def __init__(self, name, weight=1, **kwargs):
+        self.name = name
+        self.weight = weight
+
+        for key, value in kwargs.items():
+            self.setattr(key, value)
+
     @abstractmethod
     def match(self):
         pass
@@ -9,6 +26,48 @@ class Pattern(ABC):
     def filter(self):
         pass
 
-# class StringPattern(Pattern):
-# class BytePattern(Pattern):
-# class REPattern(Pattern):
+
+class ExplicitPattern(Pattern):
+    """
+    This is a subclass of Pattern for patterns that have an explicit
+    pat object, instead of a pure match function.
+    """
+    def __init__(self, name, pat, **kwargs):
+        super().__init__(name, **kwargs)
+        self.pat = pat
+
+
+class StringPattern(ExplicitPattern):
+    """
+    This is a subclass of ExplicitPattern for patterns that match
+    a single string.
+    """
+    def match(self):
+        pass
+
+    def filter(self):
+        pass
+
+
+class BytePattern(ExplicitPattern):
+    """
+    This is a subclass of ExplicitPattern for patterns that match a
+    single bytestring.
+    """
+    def match(self):
+        pass
+
+    def filter(self):
+        pass
+
+
+class REPattern(ExplicitPattern):
+    """
+    This is a subclass of ExplicitPattern for patterns that match a
+    regular expression
+    """
+    def match(self):
+        pass
+
+    def filter(self):
+        pass
