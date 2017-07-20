15b1bb4bd56b90bf726dfecdfce40bf379757b8d
cts <hvu@ciphertechsolutions.com>
2017-07-20 12:22:32 -0400

    Add more documentation and fix some typo

--- transformer.py
+++ transformer.py
@@ -2,31 +2,58 @@ from abc import ABC, abstractmethod
 
 
 class Transformer(ABC):
-    class_description = "Long Description about what the transformer does"
-    class_id = "ref_id"
+    """
+    Name: Transformer's Name
+    Description: An abstract class for all transformer
+    ID: ref_id
+    """
 
     def __init(self, value):
-        self.name = "Transformer Name"
-        self.shortName = "Trans_Name"
         self.value = value
 
     @abstractmethod
     def transform(self, data):
+        """
+        This method contains all the requires steps/calls needed
+        to transform a data. After it finishes transforming the
+        data, it needs to return a bytestring to be evulated
+
+        Args:
+            data: The data that will be decoded by this method
+
+        Returns:
+            A bytestring of the transformed data
+        """
         pass
 
     @staticmethod
     @abstractmethod
     def all_iteration():
+        """
+        This method will create a generator that lists/produces
+        all the different iteration possible that this class
+        can handle
+
+        Return:
+            A generator that produces all the different iteration
+            that this class can use to transform the string. For 
+            example:
+
+            If this class transform by XOR, this method will produce
+            the value 1 - 256 (0 is the identity value for XOR).
+        """
         pass
 
 
-class Transform_String_Empyt(Transformer):
-    class_description = "Do an empty transformation. No data is changed"
-    class_id = "no_str"
+class Transform_String_Empty(Transformer):
+    """
+    Name: No String Transform
+    Description: "Do not transform the data. This is to quickly parse the
+        original data"
+    ID: no_str
+    """
 
     def __init__(self, value):
-        self.name = "No String Transform"
-        self.shortName = "No Str Transform"
         self.value = ""
 
     def transform(self, data):
@@ -36,13 +63,15 @@ class Transform_String_Empyt(Transformer):
         yield None
 
 
-class Transform_Char_Empyt(Transformer):
-    class_description = "Do an empty transformation. No data is changed"
-    class_id = "no_char"
+class Transform_Char_Empty(Transformer):
+    """
+    Name: No Char Transform
+    Description: "Do not transform the data. This is to quickly parse the
+        original data"
+    ID: no_char
+    """
 
     def __init__(self, value):
-        self.name = "No Charter Transform"
-        self.shortName = "No Char Transform"
         self.value = ""
 
     def transform(self, data):
