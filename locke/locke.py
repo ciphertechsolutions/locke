f9e7d34db5f0b8ccb2c2b86b21396ae956954fb4
William Woodruff <wwoodruff@ciphertechsolutions.com>
2017-07-21 10:39:20 -0400

    locke: Add processes field for MP impl

--- locke/locke.py
+++ locke/locke.py
@@ -8,8 +8,9 @@ class Locke(object):
     The Locke class is used to scan a bytestring, searching for a set
     of patterns defined by Pattern subclasses.
     """
-    def __init__(self, patterns):
+    def __init__(self, patterns, processes=4):
         self.patterns = patterns
+        self.processes = processes
 
     def scan(self, data):
         for pat in self.patterns:
