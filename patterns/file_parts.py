f6df19ef5a5843b32cfbf66c306caf756139a0ee
William Woodruff <wwoodruff@ciphertechsolutions.com>
2017-07-21 14:35:22 -0400

    patterns/file_parts: Add file part patterns

---
+++ patterns/file_parts.py
@@ -0,0 +1,17 @@
+LOCKE_PATTERNS += [
+    BytePattern('OLE2 header', '\xD0\xCF\x11\xE0\xA1\xB1\x1A\xE1', weight=10),
+
+    BytePattern('VBA macros', 'VBA'),
+
+    BytePattern('Flash OLE #1', 'ShockwaveFlash.ShockwaveFlash', weight=10),
+
+    BytePattern('Flash OLE #2', 'S\x00h\x00o\x00c\x00k\x00w\x00a\x00v\x00e\x00F\x00l\x00a\x00s\x00h', weight=10),
+
+    BytePattern('PDF header', '%PDF-', weight=10),
+
+    BytePattern('PDF hooter', '%EOF', weight=10),
+
+    BytePattern('RTF header', '{\\rtf', weight=10),
+
+    BytePattern('RTF object', '{\\object', weight=10),
+]
