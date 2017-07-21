584ece04d39488e4b2a59c690400eb0622fcb106
William Woodruff <wwoodruff@ciphertechsolutions.com>
2017-07-21 09:36:41 -0400

    locke: Add initial Locke class

---
+++ locke/locke.py
@@ -0,0 +1,22 @@
+"""
+This module contains definitions for the main Locke class.
+"""
+
+
+class Locke(object):
+    """
+    The Locke class is used to scan a bytestring, searching for a set
+    of patterns defined by Pattern subclasses.
+    """
+    def __init__(self, patterns):
+        self.patterns = patterns
+
+    def scan(self, data):
+        for pat in self.patterns:
+            matches = pat.find_all(data)
+            yield pat, matches
+
+    def count(self, data):
+        for pat in self.patterns:
+            count = pat.count(data)
+            yield pat, count
