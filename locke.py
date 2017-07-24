48d42ea3f3d37b8863bafc2d6ce40dc7875a74f2
William Woodruff <wwoodruff@ciphertechsolutions.com>
2017-07-24 11:23:09 -0400

    utils: Add prettyhex2
    Remove test CSV.

--- locke.py
+++ locke.py
@@ -65,7 +65,7 @@ def search(ctx, csv, files):
         print("File: %s\n" % f.name)
         for pat, matches in l.scan(f.read()):
             for index, match in matches:
-                mstr = repr(match)
+                mstr = utils.prettyhex2(match)
                 if len(mstr) > 50:
                     mstr = mstr[:24] + '...' + mstr[-23:]
 
--- locke/utils.py
+++ locke/utils.py
@@ -1,2 +1,6 @@
 def prettyhex(bstr):
     return ''.join('\\x{:02x}'.format(b) for b in bstr)
+
+
+def prettyhex2(bstr):
+    return repr(bstr)[1:]
