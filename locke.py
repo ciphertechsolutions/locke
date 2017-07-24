a92d6c5498c756b6eb648942fd503df0c57a3339
William Woodruff <wwoodruff@ciphertechsolutions.com>
2017-07-24 10:34:05 -0400

    locke: Continuation formatting.

--- locke.py
+++ locke.py
@@ -68,25 +68,25 @@ def search(ctx, csv, files):
 
 @cli.command()
 @click.option('-l', '--level', default=2, help="Select transformers with"
-        "level 1, 2, or 3 and below")
+              "level 1, 2, or 3 and below")
 @click.option('-i', '--inclevel', type=int, help="Select transformers with"
-        "level 1, 2, or 3 and above")
+              "level 1, 2, or 3 and above")
 @click.option('-k', '--keep', default=20, help="How many transforms to save"
-        "after stage 1")
+              "after stage 1")
 @click.option('-s', '--save', default=10, help="How many transforms to save"
-        "after stage 2")
+              "after stage 2")
 @click.option('-z', '--zip', is_flag=True, help="Mark this file"
-        "as a zip file. Use --password to enter zip's password")
+              "as a zip file. Use --password to enter zip's password")
 @click.option('--password', nargs=1, help="Only works if -z is "
-        "set. Allows input of password for zip file")
+              "set. Allows input of password for zip file")
 @click.option('-d', '--display', is_flag=True, help="Display all available "
-        "transformers")
+              "transformers")
 @click.option('-p', '--profiling', is_flag=True)
 @click.option('-v', '--verbose', is_flag=True)
 @click.argument('filename', nargs=1, type=click.Path(exists=True))
 @click.pass_context
 def crack(ctx, level, inclevel, keep, save, zip, password, display,
-        profiling, filename, verbose):
+          profiling, filename, verbose):
     """
     Use patterns of interest to crack the supplied files.
     """
