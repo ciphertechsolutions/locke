1ac55774e5d2e655fa6d613f762761bc0db53eea
William Woodruff <wwoodruff@ciphertechsolutions.com>
2017-07-25 10:54:38 -0400

    exe_patterns: Add more patterns

--- patterns/exe_patterns.py
+++ patterns/exe_patterns.py
@@ -24,4 +24,37 @@ LOCKE_PATTERNS += [
     REPattern('EXE MZ followed by PE',
               r'(?s)MZ.{32,1024}PE\x00\x00',
               weight=100),
+
+    ByteListPattern('Common EXE strings',
+                    ['program', 'cannot', 'mode', 'microsoft',
+                        'kernel32', 'version', 'assembly',
+                        'xmlns', 'schemas', 'manifestVersion',
+                        'security', 'win32'],
+                    nocase=True,
+                    weight=100000),
+
+    ByteListPattern('Common Win32 function names',
+                    ['GetCurrent', 'Thread'],
+                    weight=100000,
+                    nocase=True),
+
+    ByteListPattern('Interesting Win32 function names',
+                    ['WriteFile', 'IsDebuggerPresent', 'RegSetValue',
+                        'CreateRemoteThread'],
+                    weight=10000,
+                    nocase=True),
+
+    ByteListPattern('Interesting WinSock function names',
+                    ['WSASocket', 'WSASend', 'WSARecv'],
+                    weight=10000,
+                    nocase=True),
+
+    ByteListPattern('Interesting DLLs',
+                    ['WS2_32.dll'],
+                    weight=10000,
+                    nocase=True),
+
+    ByteListPattern('Interesting registry keys',
+                    ['CurrentVersion\\Run', 'UserInit'],
+                    weight=10000),
 ]
