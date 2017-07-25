4b85773531841275ee62a8159aef6abd3bd21b31
William Woodruff <wwoodruff@ciphertechsolutions.com>
2017-07-25 12:37:21 -0400

    locke: Naive pattern multithreading

--- locke/locke.py
+++ locke/locke.py
@@ -2,20 +2,44 @@
 This module contains definitions for the main Locke class.
 """
 
+from queue import Queue
+from threading import Thread
+
+
+class PatternThread(Thread):
+    def __init__(self, queue, pattern, data):
+        super(PatternThread, self).__init__()
+        self.queue = queue
+        self.pattern = pattern
+        self.data = data
+
+    def run(self):
+        matches = self.pattern.scan(self.data)
+        self.queue.put((self.pattern, matches), block=False)
+
 
 class Locke(object):
     """
     The Locke class is used to scan a bytestring, searching for a set
     of patterns defined by Pattern subclasses.
     """
-    def __init__(self, patterns, processes=4):
+    def __init__(self, patterns):
         self.patterns = patterns
-        self.processes = processes
+        self.results = Queue()
 
     def scan(self, data):
-        for pat in self.patterns:
-            matches = pat.scan(data)
+        threads = [PatternThread(self.results, pat, data)
+                   for pat in self.patterns]
+
+        for thread in threads:
+            thread.start()
+
+        while not self.results.empty():
+            pat, matches = self.results.get()
             yield pat, matches
+
+        for thread in threads:
+            thread.join()
 
     def count(self, data):
         for pat in self.patterns:
