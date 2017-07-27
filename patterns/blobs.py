41f9889b634b5d5b307827f2e2bc80df13874020
William Woodruff <wwoodruff@ciphertechsolutions.com>
2017-07-27 14:37:05 -0400

    patterns: Document blobs classes

--- patterns/blobs.py
+++ patterns/blobs.py
@@ -1,11 +1,17 @@
 from apm.pattern_plugin import *
 
 class HexBlob(BytesPatternPlugin):
+    """
+    A pattern describing hexadecimal strings.
+    """
     NoCase = True
     Description = 'Hexadecimal string blob (>= 32 bytes)'
     Pattern = r'[A-F0-9]{32,}'
 
 
 class Base64Blob(BytesPatternPlugin):
+    """
+    A pattern describing base64 strings.
+    """
     Description = 'Base64 string blob'
     Pattern = r'(?:[A-Za-z0-9+/]{4}){2,}(?:[A-Za-z0-9+/]{2}[AEIMQUYcgkosw048]=|[A-Za-z0-9+/][AQgw]==)'
