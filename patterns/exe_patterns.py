023012684204b2cc6bf4d00e5125bc9cc25d6a1b
William Woodruff <wwoodruff@ciphertechsolutions.com>
2017-07-27 14:31:28 -0400

    patterns: Document exe_patterns classes

--- patterns/exe_patterns.py
+++ patterns/exe_patterns.py
@@ -2,34 +2,51 @@ from apm.pattern_plugin import *
 
 
 class DOSMessage(BytesPatternPlugin):
-    Description = 'DOS message'
+    """
+    The DOS message normally seen in a PE's compatibility header.
+    """
     Pattern = 'This program cannot be run in DOS mode'
     Weight = 100
 
 
 class PEHeader(BytesPatternPlugin):
+    """
+    The magic bytes for a PE file.
+    """
     Description = 'PE header magic'
     Pattern = 'PE'
 
 
 class MZHeaders(BytesListPatternPlugin):
+    """
+    The magic bytes for a MZ file.
+    """
     Description = 'EXE MZ header magics'
     Patterns = ['MZ', 'ZM']
 
 
 class PESectionNames(BytesListPatternPlugin):
+    """
+    Section names commonly found within PE files.
+    """
     NoCase = True
     Description = 'PE section names'
     Patterns = ['.text', '.data', '.rdata', '.rsrc']
 
 
 class MZFollowedByPE(REPatternPlugin):
+    """
+    The entire span of a PE compatibility header.
+    """
     Description = 'MZ header followed by PE header'
     Pattern = r'(?s)MZ.{32,1024}PE\x00\x00'
     Weight = 100
 
 
 class EXECommand(BytesListPatternPlugin):
+    """
+    Strings commonly found within PE files.
+    """
     NoCase = True
     Description = 'Common EXE strings'
     Patterns = ['program', 'cannot', 'mode', 'microsoft',
@@ -40,6 +57,9 @@ class EXECommand(BytesListPatternPlugin):
 
 
 class CommonWin32Functions(BytesListPatternPlugin):
+    """
+    Win32 API functions commonly found within files.
+    """
     NoCase = True
     Description = 'Common Win32 function names'
     Patterns = ['GetCurrent', 'Thread']
@@ -47,6 +67,9 @@ class CommonWin32Functions(BytesListPatternPlugin):
 
 
 class InterestingWin32Functions(BytesListPatternPlugin):
+    """
+    Win32 API functions that might be interesting.
+    """
     NoCase = True
     Description = 'Interesting Win32 function names'
     Patterns = ['WriteFile', 'IsDebuggerPresent',
@@ -55,6 +78,9 @@ class InterestingWin32Functions(BytesListPatternPlugin):
 
 
 class InterestingWinSockFunctions(BytesListPatternPlugin):
+    """
+    WinSock API functions that might be interesting.
+    """
     NoCase = True
     Description = 'Interesting WinSock function names'
     Patterns = ['WSASocket', 'WSASend', 'WSARecv']
@@ -62,6 +88,9 @@ class InterestingWinSockFunctions(BytesListPatternPlugin):
 
 
 class InterestingDLLs(BytesListPatternPlugin):
+    """
+    DLL filenames that might be interesting.
+    """
     NoCase = True
     Description = 'Interesting DLLs'
     Patterns = ['WS2_32.dll']
@@ -69,6 +98,9 @@ class InterestingDLLs(BytesListPatternPlugin):
 
 
 class InterestingRegKeys(BytesListPatternPlugin):
+    """
+    Windows registry keys that might be interesting.
+    """
     Description = 'Interesting registry keys'
     Patterns = ['CurrentVersion\\Run', 'UserInit']
     Weight = 10000
