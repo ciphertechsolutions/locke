8d57ab9b3b570199e8b8c4d828cb5dbc8b4f8a8c
William Woodruff <wwoodruff@ciphertechsolutions.com>
2017-07-28 11:34:39 -0400

    match: Annotate types

--- apm/match.py
+++ apm/match.py
@@ -4,7 +4,7 @@ class Match(object):
     by a pattern during the scanning of some (larger)
     data.
     """
-    def __init__(self, offset, data):
+    def __init__(self, offset: int, data: bytes):
         super().__init__()
         self.offset = offset
         self.data = data
